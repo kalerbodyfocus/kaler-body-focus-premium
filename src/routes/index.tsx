7e830b7e7b2064e37bdde484c0efbf9bc58ed334
kalerbodyfocus <kalerbodyfocus@gmail.com>
2026-06-30 09:16:49 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/site/Sections";
 import { InquiryForm } from "@/components/site/InquiryForm";
 import { getSanityContent } from "@/lib/sanity";
-import ogImage from "@/assets/logo-full(1).png";
+import ogImage from "@/assets/logo-full(1).PNG";
 
 
 export const Route = createFileRoute("/")({
